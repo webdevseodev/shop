b0d4d382a069ff6ed002bd36b1e1a4257d78a99e
sahumo <7523460@gmail.com>
2022-07-31 19:08:35 +0300

    sd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
@@ -10,6 +11,7 @@ const IndexPage = () => {
         alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
         src="../images/icon.png"
       />
+      <Link to="./proba">Proba</Link>
     </Layout>
   )
 }
