a21438293f6d41dc21d15aff273731da8ea90740
sahumo <7523460@gmail.com>
2022-07-31 18:40:30 +0300

    f

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 // Step 1: Import React
+import { Link } from 'gatsby'
 import * as React from 'react'
 
 // Step 2: Define your component
@@ -7,6 +8,7 @@ const IndexPage = () => {
     <main>
       <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   )
