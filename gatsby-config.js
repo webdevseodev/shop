accb7dfbfa5754b61f3d1f69c7903938aa29f045
sahumo <7523460@gmail.com>
2022-07-31 18:56:41 +0300

    7

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,10 @@ module.exports = {
     title: `bakalokafeneion`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+
+
+  ],
 }
